dbaf45f0fe7ca1787692634df50eda0e381eba63
gazhur94 <gazhur94@gmail.com>
2018-03-11 13:06:23 +0200

    new function

--- controllers/PlayerVSBot.php
+++ controllers/PlayerVSBot.php
@@ -9,6 +9,26 @@ class PlayerVSBot
 {
     public function __construct()
     {  
+        // if (!isset($_COOKIE['turnId']))
+        // {
+        //     setcookie('turnId',1,time()+(3600*24*365));
+             
+        //     $_SESSION['guess'] = new number;
+        // }
+
+        // if (isset($_POST['sendCowsBulls']))
+        // {
+        //     $bulls = $_POST['bulls'];
+        //     $cows = $_POST['cows'];
+
+
+        //     $guess = array();
+
+           
+
+
+
+        // }
         helpers::render('playerVSBot');
     }
 }
