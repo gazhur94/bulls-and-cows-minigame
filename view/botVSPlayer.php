e4efdca84ac43a214e81c41a84c3e66fb0ce9033
gazhur94 <gazhur94@gmail.com>
2018-03-17 14:45:47 +0200

    improved view

--- view/botVSPlayer.php
+++ view/botVSPlayer.php
@@ -23,7 +23,7 @@
 	
 		<div class="container">
 		<div class="row">
-		<div class="col-9" style="margin-left:25%">
+		<div class="col-9 offset-3" >
 			
 		
 		
@@ -32,34 +32,33 @@
 
 						<?php for ($i=1;$i<=$_COOKIE['turnId']-1;$i++): ?>
 							<div class="countdown circled small">
-							<div class="col-10 text-left" >
-								<?php 
-									$turnId = $i;
+								<div class="text-left" >
+									<?php 
+										$turnId = $i;
+										$currentTurn = unserialize($_COOKIE["turn$turnId"]);
+									?>
+
+									<?php for ($j=1;$j<=4;$j++): ?>
 									
-									$currentTurn = unserialize($_COOKIE["turn$turnId"]);
+										<div class="time days" style="text-align: center">
+											<div class="value"><?php echo $currentTurn->{'num'."$j"}; ?></div>		
+										</div>
+									<?php endfor; ?>
 
-								?>
+									<?php for ($c=0; $c<$currentTurn->cows;$c++): ?>
+											<span>
+												<img src="/view/images/cow.png" alt="cow" style="width:70px; height:70px;">
+											</span>
+									<?php endfor; ?>
 
-								<?php for ($j=1;$j<=4;$j++): ?>
-								
-									<div class="time days" style="text-align: center	">
-										<div class="value"><?php echo $currentTurn->{'num'."$j"}; ?></div>		
-									</div>
-								<?php endfor; ?>
-
-								<?php for ($c=0; $c<$currentTurn->cows;$c++): ?>
-										<span>
-											<img src="/view/images/cow.png" alt="cow" style="width:70px; height:70px;">
-										</span>
-								<?php endfor; ?>
-
-								<?php for ($b=0; $b<$currentTurn->bulls;$b++): ?>	
-										<span>
-											<img src="/view/images/bull.png" alt="bull" style="width:70px; height:70px;">
-										</span>
-								<?php endfor; ?>	
+									<?php for ($b=0; $b<$currentTurn->bulls;$b++): ?>	
+											<span>
+												<img src="/view/images/bull.png" alt="bull" style="width:70px; height:70px;">
+											</span>
+									<?php endfor; ?>	
 				
-							</div></div>
+								</div>
+							</div>
 						<?php endfor; ?>
 					<?php endif; ?>
 
@@ -69,7 +68,7 @@
 						<?php if ((isset($_POST['num1'])) && (isset($_POST['num2'])) && (isset($_POST['num3'])) && (isset($_POST['num4'])) ): ?>
 							<?php if (!isset($error)): ?>
 								<div class="countdown circled small">
-								<div class="col-10 text-left" >			
+									<div class="text-left" >			
 											<?php for ($i=1;$i<=4;$i++): ?>
 													
 												<div class="time days" style="text-align: center">
@@ -90,20 +89,18 @@
 														<img src="/view/images/bull.png" alt="bull" style="width:70px; height:70px;">
 													</span>
 																<?php endfor; ?>	
-								</div>
+									</div>
 								</div>
 							<?php endif; ?>
 						<?php endif; ?>
-					<?php endif; ?>
-
-				
+					<?php endif; ?>				
 			</div>
 		</div>
 
 	<!-- Форма вводу -->
 			<div>
 				<div class="countdown circled small">
-				<div class="col-10 text-left" style="margin-left:25%">
+				<div class="text-left" style="margin-left:25%">
 					<form method='POST'>
 						<?php if (isset($bulls)): ?>
 							<?php if ($bulls == 4): ?>
@@ -118,18 +115,18 @@
 						<?php if  (!isset($bulls) || $bulls !=4): ?>
 							<?php if ((!isset($_POST['showAnswer']))): ?>
 									
-									<?php if (isset($error)): ?>
-										<p><b style="color:red; font-size:20px; font-type:bold"><?php echo $error; ?><b></p>
-									<?php endif ?>	
+								<?php if (isset($error)): ?>
+									<p><b style="color:red; font-size:20px; font-type:bold"><?php echo $error; ?><b></p>
+								<?php endif ?>	
 
-									<?php for ($i=1;$i<=4;$i++): ?>
-										<div class="time days" style="text-align:center">
-											<div class="value"><input style ="width:40px; font-size:25px; height:45px" type="number" min="0" max="9"  autofocus="" name="<?php echo('num'."$i")  ?>" maxlength="1"></div>
-											
-										</div>
-									<?php endfor; ?>
+								<?php for ($i=1;$i<=4;$i++): ?>
+									<div class="time days" style="text-align:center">
+										<div class="value"><input style ="width:40px; font-size:25px; height:45px" type="number" min="0" max="9"  autofocus="" name="<?php echo('num'."$i")  ?>" maxlength="1"></div>
+										
+									</div>
+								<?php endfor; ?>
 
-									<input type = "submit" class="time days" style="width:180px; color:white; background-color:blue" name="send_numbers" value = "Відправити">
+								<input type = "submit" class="time days" style="width:180px; color:white; background-color:blue" name="send_numbers" value = "Відправити">
 									
 							<?php endif; ?>
 						<?php endif; ?>
--- view/playerVSBot.php
+++ view/playerVSBot.php
@@ -22,183 +22,190 @@
 	</div>
 
     <div class="splash" >
+		<div class="container">
+			<div class="row">
+				<div class="col-9 offset-3" >
 			
-		<?php if(isset($_COOKIE['turnId'])): ?>
-						
-				<?php if(isset($_POST['sendCowsBulls'])): ?>
-						<?php for ($i=1;$i<=$_COOKIE['turnId']-1;$i++): ?>
-						
-			
-						
-								
-							
-							
-								<?php 
-									$turnId = $i;
-									$turnIdbc = $i;
-									$currentTurn = unserialize($_COOKIE["turn$turnId"]); 
-								?>
-									<?php if (isset($_COOKIE["bc$turnIdbc"])): ?>
+					<?php if(isset($_COOKIE['turnId'])): ?>
 									
-										<div class="countdown circled small">
-											<?php $currentBC = unserialize($_COOKIE["bc$turnIdbc"]); ?>
+							<?php if(isset($_POST['sendCowsBulls'])): ?>
+									<?php for ($i=1;$i<=$_COOKIE['turnId']-1;$i++): ?>
+									<div class="text-left" >
+						
+									
+											
 										
+										
+											<?php 
+												$turnId = $i;
+												$turnIdbc = $i;
+												$currentTurn = unserialize($_COOKIE["turn$turnId"]); 
+											?>
+												<?php if (isset($_COOKIE["bc$turnIdbc"])): ?>
 												
-													<?php for ($j=1;$j<=4;$j++): ?>
+													<div class="countdown circled small"  style="text-align: center">
+														<?php $currentBC = unserialize($_COOKIE["bc$turnIdbc"]); ?>
 													
-													<div class="time days">
-														<div class="value"><?php echo $currentTurn->{'num'."$j"}; ?></div>		
-													</div>
-													<?php endfor; ?>
+															
+																<?php for ($j=1;$j<=4;$j++): ?>
+																
+																<div class="time days">
+																	<div class="value"><?php echo $currentTurn->{'num'."$j"}; ?></div>		
+																</div>
+																<?php endfor; ?>
 
-													<?php for ($c=0; $c<$currentBC->cows;$c++): ?>
-															<span>
-																<img src="/view/images/cow.png" alt="cow" style="width:70px; height:70px;">
-															</span>
-													<?php endfor; ?>
+																<?php for ($c=0; $c<$currentBC->cows;$c++): ?>
+																		<span>
+																			<img src="/view/images/cow.png" alt="cow" style="width:70px; height:70px;">
+																		</span>
+																<?php endfor; ?>
 
-													<?php for ($b=0; $b<$currentBC->bulls;$b++): ?>	
-															<span>
-																<img src="/view/images/bull.png" alt="bull" style="width:70px; height:70px;">
-															</span>
-													<?php endfor; ?>
-												
-										</div>	
-									<?php else: ?>
+																<?php for ($b=0; $b<$currentBC->bulls;$b++): ?>	
+																		<span>
+																			<img src="/view/images/bull.png" alt="bull" style="width:70px; height:70px;">
+																		</span>
+																<?php endfor; ?>
+															
+													</div>	
+												<?php else: ?>
+													<div class="countdown circled small"  style="text-align: center">
+															
+														<?php if (!isset($error)): ?>
+
+															<?php for ($j=1;$j<=4;$j++): ?>
+															
+															<div class="time days">
+																<div class="value"><?php echo $currentTurn->{'num'."$j"}; ?></div>		
+															</div>
+															<?php endfor; ?>
+
+															<?php for ($c=0; $c<$cows;$c++): ?>
+																	<span>
+																		<img src="/view/images/cow.png" alt="cow" style="width:70px; height:70px;">
+																	</span>
+															<?php endfor; ?>
+
+															<?php for ($b=0; $b<$bulls;$b++): ?>	
+																	<span>
+																		<img src="/view/images/bull.png" alt="bull" style="width:70px; height:70px;">
+																	</span>
+															<?php endfor; ?>
+														<?php endif; ?>
+														
+													</div>	
+												<?php endif; ?>
+
+									</div>
+									<?php endfor; ?>
+									
+										
+
+								<?php else: ?>
+									<?php for ($i=1;$i<=$_COOKIE['turnId']-1;$i++): ?>		
 										<div class="countdown circled small">
+										
+											<?php 
+												$turnId = $i;
+												$turnIdbc = $i;
+												$currentTurn = unserialize($_COOKIE["turn$turnId"]);
+												$currentBC = unserialize($_COOKIE["bc$turnIdbc"]);
 												
-											<?php if (!isset($error)): ?>
+											?>
 
-												<?php for ($j=1;$j<=4;$j++): ?>
-												
+											<?php for ($j=1;$j<=4;$j++): ?>
+											
 												<div class="time days">
 													<div class="value"><?php echo $currentTurn->{'num'."$j"}; ?></div>		
 												</div>
-												<?php endfor; ?>
+											<?php endfor; ?>
 
-												<?php for ($c=0; $c<$cows;$c++): ?>
-														<span>
-															<img src="/view/images/cow.png" alt="cow" style="width:70px; height:70px;">
-														</span>
-												<?php endfor; ?>
+											<?php for ($c=0; $c<$currentBC->cows;$c++): ?>
+													<span>
+														<img src="/view/images/cow.png" alt="cow" style="width:70px; height:70px;">
+													</span>
+											<?php endfor; ?>
 
-												<?php for ($b=0; $b<$bulls;$b++): ?>	
-														<span>
-															<img src="/view/images/bull.png" alt="bull" style="width:70px; height:70px;">
-														</span>
-												<?php endfor; ?>
-											<?php endif; ?>
-											
-										</div>	
-									<?php endif; ?>
-
-
-						<?php endfor; ?>
+											<?php for ($b=0; $b<$currentBC->bulls;$b++): ?>	
+													<span>
+														<img src="/view/images/bull.png" alt="bull" style="width:70px; height:70px;">
+													</span>
+											<?php endfor; ?>
+										</div>
+									<?php endfor; ?>
 						
+								<?php endif; ?>
+
 							
-
-					<?php else: ?>
-						<?php for ($i=1;$i<=$_COOKIE['turnId']-1;$i++): ?>		
-							<div class="countdown circled small">
-							
-								<?php 
-									$turnId = $i;
-									$turnIdbc = $i;
-									$currentTurn = unserialize($_COOKIE["turn$turnId"]);
-									$currentBC = unserialize($_COOKIE["bc$turnIdbc"]);
-									
-								?>
-
-								<?php for ($j=1;$j<=4;$j++): ?>
-								
-									<div class="time days">
-										<div class="value"><?php echo $currentTurn->{'num'."$j"}; ?></div>		
-									</div>
-								<?php endfor; ?>
-
-								<?php for ($c=0; $c<$currentBC->cows;$c++): ?>
-										<span>
-											<img src="/view/images/cow.png" alt="cow" style="width:70px; height:70px;">
-										</span>
-								<?php endfor; ?>
-
-								<?php for ($b=0; $b<$currentBC->bulls;$b++): ?>	
-										<span>
-											<img src="/view/images/bull.png" alt="bull" style="width:70px; height:70px;">
-										</span>
-								<?php endfor; ?>
-							</div>
-						<?php endfor; ?>
-			
 					<?php endif; ?>
 
-				
-		<?php endif; ?>
-
-		
-
-	
-		<div class="countdown text-left  circled small">
-				<form method='POST'>			
-				
-					<?php if(!isset($error)): ?>
-						<?php if(isset($_SESSION['turnId'])): ?>
-							<div class="countdown circled small">
-							
-							
-								<?php for ($j=1;$j<=4;$j++): ?>
-									<div class="time days">
-										<div class="value"><?php echo ${'num'."$j"} ?></div>		
-									</div>
-								<?php endfor; ?>
-
-								<?php if (!isset($_POST['win']) && !isset($_POST['loose'])):	 ?>		
-									<input type = "submit" class="time days" style="width:140px; color:white; background-color:green" name="win" value = "Вгадав">
-									<input type = "submit" class="time days" style="width:170px; color:white; background-color:red" name="loose" value = "Не вгадав">
-								<?php endif; ?>			
-					<?php endif; ?>
 					
 
-	
-				</form>					
-				<?php endif; ?>	
+				
+					<div class="countdown text-left  circled small">
+							<form method='POST'>			
+							
+								<?php if(!isset($error)): ?>
+									<?php if(isset($_SESSION['turnId'])): ?>
+										<div class="countdown circled small">
+										
+										
+											<?php for ($j=1;$j<=4;$j++): ?>
+												<div class="time days">
+													<div class="value"><?php echo ${'num'."$j"} ?></div>		
+												</div>
+											<?php endfor; ?>
+
+											<?php if (!isset($_POST['win']) && !isset($_POST['loose'])):	 ?>		
+												<input type = "submit" class="time days" style="width:140px; color:white; background-color:green" name="win" value = "Вгадав">
+												<input type = "submit" class="time days" style="width:170px; color:white; background-color:red" name="loose" value = "Не вгадав">
+											<?php endif; ?>			
+								<?php endif; ?>
+								
+
+				
+							</form>					
+							<?php endif; ?>	
+					</div>
+
+					<?php if (isset($error)): ?>
+						<p><b style="color:red; font-size:20px; font-type:bold"><?php echo $error; ?><b></p>
+					<?php endif ?>
+					
+					<?php if ((isset($_POST['loose'])) || (isset($_POST['sendCowsBulls']) && (isset($error)))) : ?>
+						<div class="countdown text-left  circled small">
+							<form method='POST'>
+								<img src="/view/images/cow.png" alt="cow" style="width:70px; height:70px;">
+								<b style="color:white; font-size:40px; font-type:bold">: </b>
+								<input style ="width:40px; font-size:25px" type="number" min="0" max="4"  autofocus="" name="cows" maxlength="1">
+								<img src="/view/images/bull.png" alt="bull" style="width:70px; height:70px;">
+								<b style="color:white; font-size:40px; font-type:bold">: </b>
+								<input style ="width:40px; font-size:25px" type="number" min="0" max="4"  name="bulls" maxlength="1">
+								<input type = "submit" class="time days" style="width:180px; color:white" name="sendCowsBulls" value = "Відправити">
+							</form>	
+						</div>
+					<?php endif; ?>
+
+					</div>
+					</div>
+					</div>	
+
+					<?php if (isset($_POST['win']) || (isset($_POST['bulls']) && $_POST['bulls'] == 4)): ?>
+						<div class="countdown text-left  circled small">
+							<p><b style="color:green; font-size:40px; font-type:bold">Я вгадав<b></p>
+						</div>
+					<?php endif; ?>	
+					
+					
+					<form method='POST'>			
+						<div class="countdown circled small" style="text-align:center">
+									<br><br>
+									<input type = "submit" class="time days" style="width:240px; color:white" name="goToMain" value = "Головне меню">
+									<input type = "submit" class="time days" style="width:170px; color:white" name="resetGame2" value = "Спочатку">
+						</div>
+					</form>		
+				</div>
+			</div>
 		</div>
-
-		<?php if (isset($error)): ?>
-			<p><b style="color:red; font-size:20px; font-type:bold"><?php echo $error; ?><b></p>
-		<?php endif ?>
-		
-		<?php if ((isset($_POST['loose'])) || (isset($_POST['sendCowsBulls']) && (isset($error)))) : ?>
-			<div class="countdown text-left  circled small">
-				<form method='POST'>
-					<img src="/view/images/cow.png" alt="cow" style="width:70px; height:70px;">
-					<b style="color:white; font-size:40px; font-type:bold">: </b>
-					<input style ="width:40px; font-size:25px" type="number" min="0" max="4"  autofocus="" name="cows" maxlength="1">
-					<img src="/view/images/bull.png" alt="bull" style="width:70px; height:70px;">
-					<b style="color:white; font-size:40px; font-type:bold">: </b>
-					<input style ="width:40px; font-size:25px" type="number" min="0" max="4"  name="bulls" maxlength="1">
-					<input type = "submit" class="time days" style="width:180px; color:white" name="sendCowsBulls" value = "Відправити">
-				</form>	
-			</div>
-		<?php endif; ?>
-			
-
-		<?php if (isset($_POST['win']) || (isset($_POST['bulls']) && $_POST['bulls'] == 4)): ?>
-			<div class="countdown text-left  circled small">
-				<p><b style="color:green; font-size:40px; font-type:bold">Я вгадав<b></p>
-			</div>
-		<?php endif; ?>	
-		
-		
-        <form method='POST'>			
-			<div class="countdown circled small" style="text-align:center">
-						<br><br>
-						<input type = "submit" class="time days" style="width:240px; color:white" name="goToMain" value = "Головне меню">
-						<input type = "submit" class="time days" style="width:170px; color:white" name="resetGame2" value = "Спочатку">
-			</div>
-		</form>		
-		
-		
 	</div>
 	
 	
