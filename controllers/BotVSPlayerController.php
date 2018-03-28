7aa300ab574b90916145d31239a91aae823670cc
gazhur94 <gazhur94@gmail.com>
2018-03-28 17:22:41 +0300

    deleted unnecessary comments

--- controllers/BotVSPlayerController.php
+++ controllers/BotVSPlayerController.php
@@ -73,52 +73,6 @@ class BotVSPlayerController
     }
 
 
-    //     if (!isset($_COOKIE['turnId']))
-    //     {
-    //         setcookie('turnId',1,time()+(3600));
-             
-    //         $_SESSION['number'] = new number;
-    //     }
-
-    //     else
-    //     {
-
-    //         if (isset($_POST['send_numbers']))
-    //         {
-    //             //var_dump($_SESSION['number']);
-    //             $errors = new errors;
-
-    //             if (empty($errors->error))
-    //             {
-    //                 $turnId = $_COOKIE['turnId'];
-    //                 ${"turn$turnId"} = new turn;
-                    
-    //                 $bulls = ${"turn$turnId"}->bulls;
-    //                 $cows = ${"turn$turnId"}->cows;
-                    
-    //                 setcookie("turn$turnId",serialize(${"turn$turnId"}),time()+(3600));
-    //                 setcookie('turnId',$_COOKIE['turnId']+1,time()+(3600));
-
-    //                 return helpers::render('botVSPlayer', ["bulls" => "$bulls", "cows" =>"$cows"]);
-                    
-
-
-    //             }
-    //             else
-    //             {
-                    
-                    
-    //                 return helpers::render('botVSPlayer',["error" => "$errors->error"]);
-                    
-    //             }    
-    //         }
-    //     }
-      
-    //     return helpers::render('botVSPlayer');
-
-    // }
-    
-
 }
 
 
