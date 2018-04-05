4399bf93c634490c8339433a79e7463cc3cc1243
gazhur94 <gazhur94@gmail.com>
2018-04-05 18:52:06 +0300

    deleted form

--- view/mainpage.php
+++ view/mainpage.php
@@ -2,14 +2,14 @@
 
 <?php helpers::render('layouts/header'); ?>
 		
-        <form method='POST'>			
-            <div class="countdown circled small" style="text-align:center">
-                <a href="/botVSplayer" class="time days" style="width:320px; color:white" >Відгадати число бота</a>
-            </div>
-			<div class="countdown circled small" style="text-align:center">
-				<a href="/playerVSbot" class="time days" style="width:320px; color:white" >Загадати боту число</a>
-            </div>
-		</form>		
+        			
+    <div class="countdown circled small" style="text-align:center">
+        <a href="/botVSplayer" class="time days" style="width:320px; color:white" >Відгадати число бота</a>
+    </div>
+    <div class="countdown circled small" style="text-align:center">
+        <a href="/playerVSbot" class="time days" style="width:320px; color:white" >Загадати боту число</a>
+    </div>
+			
 		
 		
 <?php helpers::render('layouts/footer'); ?>
